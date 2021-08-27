bd82ac4969fd7bee71967007d3d4795252dfc94a
Shaun <s-purslow@hotmail.com>
2021-08-27 12:57:26 -0600

    make comments for gameplan, render header component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,10 +7,10 @@ import './styles/App.scss';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World!</h1>
+      {/* Header */}
         <Header />
-      </header>
+        {/* search */}
+        {/*Footer  */}
     </div>
   );
 }
