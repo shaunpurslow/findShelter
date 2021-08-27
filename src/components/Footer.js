bcabda8e1e17929ff5b0eb0182e0c07d7d0d236d
Shaun <s-purslow@hotmail.com>
2021-08-26 20:44:32 -0600

    add base folder and components

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Footer.module.scss'
+
+const Footer = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Header.module.scss'
+
+const Header = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Header
