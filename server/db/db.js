458cecb14726ad74d831f49d7bcf328ccfd10b64
ricardowgomes <ricardowgomes@outlook.com>
2021-08-27 16:41:26 -0600

    schema and elephantSQL2

--- server/db/db.js
+++ server/db/db.js
@@ -9,12 +9,4 @@ client.connect((err) => {
   }
 
   console.log('connecting to elephantSQL')
-  // client.query('SELECT * FROM guests', (err, result) => {
-  //   if (err) {
-  //     return console.error('error running query', err);
-  //   }
-  //   console.log(result);
-  //   // >> output: 2018-08-23T14:02:57.117Z
-  //   client.end();
-  // });
 });
