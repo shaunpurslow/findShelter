9a6a5e703366d8de0c1b86fc1f8141af615f0889
Shaun <s-purslow@hotmail.com>
2021-08-27 17:42:15 -0600

    update Search component

--- client/src/components/Search.tsx
+++ client/src/components/Search.tsx
@@ -1,9 +1,18 @@
-import '.././styles/Header.scss';
+import '.././styles/Search.scss';
+import SearchIcon from '@material-ui/icons/Search';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
 
 const Search = () => {
   return (
     <main>
-      
+      <div className='container'>
+        <div className='search__bar' >
+          <SearchIcon />
+          <input type="text" placeholder='Enter your location' />
+          <LocationOnIcon />
+        </div>
+      </div>
+      <button className='container__submit-btn' >Search</button>
     </main>
   )
 }
