aebdb5448a90c4e019e47e7bd242060f809afa2a
ricardowgomes <ricardowgomes@outlook.com>
2021-08-27 12:33:57 -0600

    Header component test

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,12 @@
+import '.././styles/Header.scss';
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <h1>Header Component</h1>
+    </>
+  )
+}
+
+export default Header
