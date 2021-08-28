2c0311095c99790417ed2b47e581d2f89b5268bd
Shaun <s-purslow@hotmail.com>
2021-08-27 18:31:02 -0600

    put in logo in footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,13 +1,14 @@
 import '.././styles/Footer.scss';
-import HomeIcon from '@material-ui/icons/Home';
+// import HomeIcon from '@material-ui/icons/Home';
 const Footer = () => {
   return (
     <footer>
       <div className='copyright'>
         <p>&copy; 2021 |</p>
         <div className='footer__logo'>
-          <HomeIcon />
-          <p>findShelter</p>
+          {/* <HomeIcon />
+          <p>findShelter</p> */}
+          <img src="/img/find-shelter.png" alt="" />
         </div>
       </div>
     </footer>
