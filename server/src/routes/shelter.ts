5caff0dc6c8e309c9f392610db288173d1ad3d8f
Emmanuel Etti <emmanueletti@gmail.com>
2021-08-28 09:10:24 -0700

    initial branch commit

--- server/src/routes/shelter.ts
+++ server/src/routes/shelter.ts
@@ -7,16 +7,57 @@ import express from 'express';
 const router = express.Router();
 
 // exports a function that ultimate returns a configured router
+// db connection provided using dependency injection pattern for increased testability
 module.exports = (dbconn) => {
   router.get('/', (req, res) => {
     dbconn
-      .query('SELECT * FROM shelters')
+      .query('SELECT * FROM shelters;')
       .then((data) => res.send(data.rows))
       .catch((e) => res.status(500).json({ error: e.message }));
   });
 
-  router.get('', (req, res) => {});
-  router.get('', (req, res) => {});
+  router.get('/search', (req, res) => {
+    // TODO: move type definitions to seperate file
+    console.log(req.query);
+    interface ISearchQueryParams {
+      city?: string;
+      province?: string;
+    }
+    const { city, province }: ISearchQueryParams = req.query;
+    const values: string[] = [`%${city}%`];
+    let query: string = `
+        SELECT *
+        FROM shelters
+        WHERE LOWER(city) LIKE LOWER($1)
+      `;
+
+    if (province) {
+      query += `AND LOWER(province) LIKE LOWER($2)`;
+      values.push(province);
+    }
+
+    // make database requests
+    dbconn
+      .query(query, values)
+      .then((data) => res.send(data.rows))
+      .catch((e) => res.status(500).json({ error: e.message }));
+  });
+
+  router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    dbconn
+      .query(
+        `
+        SELECT *
+        FROM shelters
+        WHERE id = $1
+        `,
+        [id]
+      )
+      .then((data) => res.send(data.rows))
+      .catch((e) => res.status(500).json({ error: e.message }));
+  });
+
   router.patch('', (req, res) => {});
   router.post('', (req, res) => {});
   router.delete('', (req, res) => {});
