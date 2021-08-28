2f78eb6b5ce4aa087fd7a4653362886f33bb4c81
Shaun <s-purslow@hotmail.com>
2021-08-27 19:38:08 -0600

    implement react route to search and register components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,19 +3,31 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Search from './components/Search';
 import './styles/App.scss';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Register from './components/Register';
 
 function App() {
   return (
-    <div className='App'>
-      {/* Header */}
-      <Header />
-      {/* search */}
-      <Search />
-      {/* Register */}
-      
-      {/*Footer  */}
-      <Footer />
-    </div>
+    <Router>
+      <div className='App'>
+        {/* Header */}
+        <Header />
+        <Switch>
+          {/* Search */}
+          <Route exact path='/'>
+            <Search />
+          </Route>
+
+          {/* Register */}
+          <Route path='/register'>
+            <Register />
+          </Route>
+        </Switch>
+
+        {/*Footer  */}
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
