73a8694c2a462f9befbe55b384c5012f1026ff12
Emmanuel Etti <emmanueletti@gmail.com>
2021-09-04 17:15:56 -0700

    adds ability to show a shelter card when a marker on the map is clicked

--- client/src/components/mapSearch/index.tsx
+++ client/src/components/mapSearch/index.tsx
@@ -1,24 +1,15 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
+import classNames from 'classnames';
 import ReactMapGL, { Marker, FlyToInterpolator } from 'react-map-gl';
 import axios from 'axios';
 import RoomIcon from '@material-ui/icons/Room';
+import './styles.scss';
 
-const getViewportLocation = (query, cb) => {
-  axios
-    .get(
-      `http://api.positionstack.com/v1/forward?access_key=${process.env.REACT_APP_POSITION_KEY}&query=${query}`
-    )
-    .then((res) => {
-      const initialViewportLatitude = res.data.data[0].latitude;
-      const initialViewportLongitude = res.data.data[0].longitude;
-
-      cb((prev) => ({
-        ...prev,
-        latitude: initialViewportLatitude,
-        longitude: initialViewportLongitude,
-      }));
-    })
-    .catch((err) => console.log(err));
+// import {} from './styles';
+const getViewportLocation = (query) => {
+  return axios.get(
+    `http://api.positionstack.com/v1/forward?access_key=${process.env.REACT_APP_POSITION_KEY}&query=${query}`
+  );
 };
 // PROPS
 // 1 - an array of shelters with addresses to render
@@ -33,39 +24,48 @@ const mockShelters = [
   {
     id: 1,
     address: '1445 14th Ave W Vancouver BC V6H 1R5',
+    capacity: 100,
+    confirmedReservations: 95,
   },
   {
     id: 2,
     address: '1990 Panorama Dr North Vancouver BC V7G 1V1',
+    capacity: 100,
+    confirmedReservations: 100,
   },
   {
     id: 3,
     address: '302 590 12th Ave W Vancouver BC V5Z 1M2',
+    capacity: 100,
+    confirmedReservations: 90,
   },
   {
     id: 4,
     address: '1772 W 15th Vancouver BC V6J 2K8',
+    capacity: 100,
+    confirmedReservations: 100,
   },
   {
     id: 5,
     address: '1633 MacKay Ave 416 North Vancouver BC V7P 0A2',
+    capacity: 100,
+    confirmedReservations: 50,
   },
   {
     id: 6,
     address: '4402 Oxford St Burnaby BC V5C 1E5',
+    capacity: 100,
+    confirmedReservations: 50,
   },
   {
     id: 7,
     address: '242 6th St E North Vancouver BC V7L 1P5',
+    capacity: 100,
+    confirmedReservations: 50,
   },
 ];
 
 const MapSearch = () => {
-  // turn the initial city to coordinates
-
-  const [location, setLocation] = useState();
-  const [latitude, setLatitude] = useState(0);
-  const [longitude, setLongitude] = useState(0);
   const [viewport, setViewport] = useState({
     latitude: 0,
     longitude: 0,
@@ -77,7 +77,18 @@ const MapSearch = () => {
   // turn location to coordinates on initial load for the map viewport
   useEffect(() => {
     const viewportLocationQuery = `${mockInitialLocation.city}, ${mockInitialLocation.province}`;
-    getViewportLocation(viewportLocationQuery, setViewport);
+    getViewportLocation(viewportLocationQuery)
+      .then((res) => {
+        const initialViewportLatitude = res.data.data[0].latitude;
+        const initialViewportLongitude = res.data.data[0].longitude;
+
+        setViewport((prev) => ({
+          ...prev,
+          latitude: initialViewportLatitude,
+          longitude: initialViewportLongitude,
+        }));
+      })
+      .catch((err) => console.log(err));
   }, []);
 
   // loop through shelters data and and store information with coordinates in localstorage
@@ -102,6 +113,7 @@ const MapSearch = () => {
       );
     });
 
+    // can save in alternate place than localstorage
     Promise.all(getShelterCoordsPromises)
       .then(() => {
         localStorage.setItem(
@@ -116,55 +128,73 @@ const MapSearch = () => {
   const shelterDataWithCoords =
     localStorage.getItem('sheltersWithCoords') || '';
   const parsedShelterDataWithCoords = JSON.parse(shelterDataWithCoords);
-  const shelterMapMarkers = parsedShelterDataWithCoords.map((shelter) => (
-    <Marker
-      key={shelter.id}
-      latitude={shelter.latitude}
-      longitude={shelter.longitude}
-      offsetLeft={-20}
-      offsetTop={-10}>
-      <RoomIcon />
-    </Marker>
-  ));
 
-  // Directions API
+  const shelterMapMarkers = parsedShelterDataWithCoords.map((shelter) => {
+    const checkIfShelterIsFull =
+      (shelter.capacity - shelter.confirmedReservations) / shelter.capacity;
 
-  const handleChange = (e) => {
-    setLocation((prev) => e.target.value);
-  };
+    const markerStyle = checkIfShelterIsFull
+      ? 'shelter-marker'
+      : 'shelter-marker--full';
 
-  const handleClick = (e) => {
-    axios
-      .get(
-        `http://api.positionstack.com/v1/forward?access_key=${process.env.REACT_APP_POSITION_KEY}&query=${location}`
-      )
-      .then((response) => {
-        setLatitude(response.data.data[0].latitude);
-        setLongitude(response.data.data[0].longitude);
-        setViewport((prev) => ({
-          ...prev,
-          latitude: response.data.data[0].latitude,
-          longitude: response.data.data[0].longitude,
-          transitionInterpolator: new FlyToInterpolator({ speed: 1.9 }),
-          transitionDuration: 'auto',
-          zoom: 10,
-          scrollSpeed: 50,
-        }));
-      })
-      .catch((err) => console.log(err));
+    return (
+      <Marker
+        key={shelter.id}
+        latitude={shelter.latitude}
+        longitude={shelter.longitude}
+        offsetLeft={-20}
+        offsetTop={-10}
+        className={markerStyle}
+        captureClick={false}
+        capturePointerMove={false}>
+        <RoomIcon fontSize='large' />
+      </Marker>
+    );
+  });
+
+  const [selectedShelter, setSelectedShelter] = useState({});
+
+  const [showCard, setShowCard] = useState(false);
+  const handleMapClick = (e) => {
+    if (e.target.closest('path')) {
+      // make the card display
+      setShowCard((prev) => true);
+    } else {
+      setShowCard((prev) => false);
+    }
   };
 
   return (
     <div>
       <ReactMapGL
         {...viewport}
-        mapStyle='mapbox://styles/shaunp/ckt1yp4sc1kul17s1v99jevj9'
+        // mapStyle='mapbox://styles/shaunp/ckt1yp4sc1kul17s1v99jevj9'
+        mapStyle='mapbox://styles/mapbox/dark-v9'
         onViewportChange={(viewport) => setViewport(viewport)}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-        transitionInterpolator={new FlyToInterpolator()}>
+        transitionInterpolator={new FlyToInterpolator()}
+        onClick={handleMapClick}
+        getCursor={(e) => 'grab'}>
         {shelterMapMarkers}
-        <input type='text' value={location} onChange={handleChange}></input>
-        <button onClick={handleClick}>Enter</button>
+
+        {showCard && (
+          <div className='shelter-card'>
+            <div>
+              <h1 className='shelter-card__title'>Women and Family Shelter</h1>
+              <p>Phone: 604-777-7777</p>
+              <p>Website: www.shelter.ca</p>
+              <p>Address: 604 Calgary Street, Vancouver BC</p>
+              <button className='shelter-card__directions-btn'>
+                Get Directions
+              </button>
+            </div>
+            <div className='shelter-card__info'>
+              <h1>25</h1>
+              <p>Beds</p>
+              <p>Available</p>
+            </div>
+          </div>
+        )}
       </ReactMapGL>
     </div>
   );
